3d5bfca6c3beeff602cfa3d4147428816f637480
James Dunne <james.jdunne@gmail.com>
2017-11-29 18:49:52 -0600

    Working send/receive loopback test.

--- main.go
+++ main.go
@@ -2,14 +2,17 @@
 package main
 
 import (
+	"encoding/hex"
+	"fmt"
 	"net"
-	"os"
+	"time"
 
 	"github.com/urfave/cli"
 )
 
 func main() {
 	address := ""
+	datagramSize := 1500
 
 	app := cli.NewApp()
 
@@ -27,6 +30,11 @@ func main() {
 			Usage:       "UDP multicast group for transfers",
 			Destination: &address,
 		},
+		cli.IntFlag{
+			Name:        "datagram size,s",
+			Value:       1500,
+			Destination: &datagramSize,
+		},
 	}
 	app.Commands = []cli.Command{
 		cli.Command{
@@ -36,13 +44,68 @@ func main() {
 			Action: func(c *cli.Context) error {
 				//local := c.Args().First()
 				udpAddr, err := net.ResolveUDPAddr("udp", address)
+				if err != nil {
+					return err
+				}
+
 				conn, err := net.ListenMulticastUDP("udp", nil, udpAddr)
+				if err != nil {
+					return err
+				}
+
+				conn.SetReadBuffer(datagramSize)
+				buf := make([]byte, datagramSize)
+
+				// Read UDP messages from multicast:
+				for {
+					// TODO: use second parameter *net.UDPAddr to authenticate source?
+					n, _, err := conn.ReadFromUDP(buf)
+					if err != nil {
+						return err
+					}
+					msg := buf[:n]
+					fmt.Printf("%s", hex.Dump(msg))
+				}
+
+				err = conn.Close()
+				return err
+			},
+		},
+		cli.Command{
+			Name:    "serve",
+			Aliases: []string{"s"},
+			Usage:   "server files to a multicast group",
+			Action: func(c *cli.Context) error {
+				//local := c.Args().First()
+				udpAddr, err := net.ResolveUDPAddr("udp", address)
+				if err != nil {
+					return err
+				}
+
+				conn, err := net.DialUDP("udp", nil, udpAddr)
+				if err != nil {
+					return err
+				}
+
+				conn.SetWriteBuffer(datagramSize)
+				msg := []byte("hello, world!\n")
+
+				// Write UDP messages to multicast:
+				for {
+					_, err := conn.Write(msg)
+					if err != nil {
+						return err
+					}
+					fmt.Printf("%s", hex.Dump(msg))
+					time.Sleep(1 * time.Second)
+				}
+
 				err = conn.Close()
 				return err
 			},
 		},
 	}
 
-	app.Run(os.Args)
+	app.RunAndExitOnError()
 	return
 }
