163512d1d9fd1637fbb0637ef285c8221cdea772
James Dunne <james.jdunne@gmail.com>
2017-12-04 14:53:59 -0600

    Added Close() method and defined new interface types for WriterAtCloser and ReaderAtCloser. Added path validation.

--- tarball.go
+++ tarball.go
@@ -3,13 +3,27 @@ package main
 
 import (
 	"errors"
+	"io"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 var (
 	ErrOutOfRange = errors.New("Offset out of range")
 	ErrNilBuffer  = errors.New("nil buffer")
+	ErrBadPAth    = errors.New("bad path")
 )
+
+type ReaderAtCloser interface {
+	io.ReaderAt
+	io.Closer
+}
+
+type WriterAtCloser interface {
+	io.WriterAt
+	io.Closer
+}
 
 type TarballFile struct {
 	Path string
@@ -21,7 +35,8 @@ type tarballFile struct {
 	TarballFile
 
 	offset int64
-	f      *os.File
+	writer WriterAtCloser
+	reader ReaderAtCloser
 }
 
 type Tarball struct {
@@ -29,15 +44,27 @@ type Tarball struct {
 	size  int64
 }
 
-func NewTarball(files []TarballFile) *Tarball {
+func NewTarball(files []TarballFile) (*Tarball, error) {
 	filesInternal := make([]tarballFile, 0, len(files))
 
 	size := int64(0)
 	for _, f := range files {
+		// Validate paths:
+		if filepath.IsAbs(f.Path) {
+			return nil, ErrBadPAth
+		}
+		s := strings.Split(f.Path, string(filepath.Separator))
+		for _, p := range s {
+			if p == "." || p == ".." {
+				return nil, ErrBadPAth
+			}
+		}
+
 		filesInternal = append(filesInternal, tarballFile{
 			TarballFile: f,
 			offset:      size,
-			f:           nil,
+			writer:      nil,
+			reader:      nil,
 		})
 		size += f.Size
 	}
@@ -45,7 +72,26 @@ func NewTarball(files []TarballFile) *Tarball {
 	return &Tarball{
 		files: filesInternal,
 		size:  size,
+	}, nil
+}
+
+// io.Closer:
+func (t *Tarball) Close() error {
+	for _, tf := range t.files {
+		if tf.reader != nil {
+			err := tf.reader.Close()
+			if err != nil {
+				return err
+			}
+		}
+		if tf.writer != nil {
+			err := tf.writer.Close()
+			if err != nil {
+				return err
+			}
+		}
 	}
+	return nil
 }
 
 // io.WriterAt:
@@ -65,20 +111,20 @@ func (t *Tarball) WriteAt(buf []byte, offset int64) (int, error) {
 			continue
 		}
 
-		var err error
-
 		// Create file if not existing:
-		if tf.f == nil {
-			tf.f, err = os.OpenFile(tf.Path, os.O_WRONLY|os.O_CREATE, os.FileMode(tf.Mode))
+		if tf.writer == nil {
+			f, err := os.OpenFile(tf.Path, os.O_WRONLY|os.O_CREATE, os.FileMode(tf.Mode))
 			if err != nil {
 				return 0, err
 			}
 
 			// Reserve disk space:
-			err = tf.f.Truncate(tf.Size)
+			err = f.Truncate(tf.Size)
 			if err != nil {
 				return 0, err
 			}
+
+			tf.writer = f
 		}
 
 		localOffset := offset - tf.offset
@@ -90,7 +136,7 @@ func (t *Tarball) WriteAt(buf []byte, offset int64) (int, error) {
 		}
 		if len(p) > 0 {
 			// NOTE: we allow len(p) == 0 to create file as a side effect in case that's useful.
-			n, err := tf.f.WriteAt(p, localOffset)
+			n, err := tf.writer.WriteAt(p, localOffset)
 			if err != nil {
 				return 0, err
 			}
--- tarball_test.go
+++ tarball_test.go
@@ -1,6 +1,51 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+func createTarball(t *testing.T, files []TarballFile) *Tarball {
+	tb, err := NewTarball(files)
+	if err != nil {
+		t.Fatalf("NewTarball: %v", err)
+	}
+	return tb
+}
+
+func closeTarball(t *testing.T, tb *Tarball) {
+	err := tb.Close()
+	if err != nil {
+		t.Fatalf("Error closing: %v", err)
+	}
+
+	for _, f := range tb.files {
+		os.Remove(f.Path)
+	}
+}
+
+func TestTarball_Nop(t *testing.T) {
+	files := []TarballFile{}
+
+	tb := createTarball(t, files)
+	defer closeTarball(t, tb)
+}
+
+func TestTarball_BadPath1(t *testing.T) {
+	files := []TarballFile{
+		TarballFile{
+			Path: "../test.txt",
+		},
+	}
+
+	_, err := NewTarball(files)
+	if err == nil {
+		t.Fatal("Expected non-nil error")
+	}
+	if err != ErrBadPAth {
+		t.Fatal("Expected ErrBadPath")
+	}
+}
 
 func TestWriteAt_OneFile(t *testing.T) {
 	files := []TarballFile{
@@ -11,7 +56,9 @@ func TestWriteAt_OneFile(t *testing.T) {
 		},
 	}
 
-	tb := NewTarball(files)
+	tb := createTarball(t, files)
+	defer closeTarball(t, tb)
+
 	n, err := tb.WriteAt([]byte("hi\n"), 0)
 	if err != nil {
 		t.Fatal(err)
@@ -35,7 +82,9 @@ func TestWriteAt_SpanningFiles(t *testing.T) {
 		},
 	}
 
-	tb := NewTarball(files)
+	tb := createTarball(t, files)
+	defer closeTarball(t, tb)
+
 	n, err := tb.WriteAt([]byte("Hello, world!\n"), 0)
 	if err != nil {
 		t.Fatal(err)
