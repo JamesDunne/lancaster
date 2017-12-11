8c1467660c9576cdaa07910ac6c070324ac26705
James Dunne <james.jdunne@gmail.com>
2017-12-10 21:56:50 -0600

    Remove debugging print

--- server.go
+++ server.go
@@ -318,13 +318,13 @@ func (s *Server) processControl(ctrl UDPMessage) error {
 			// ACK region:
 			s.nakRegions.Ack(ack.start, ack.endEx)
 		}
+		// Record known NAKs:
 		i := 16
 		for i < len(data) {
 			v1, n := binary.Uvarint(data[i:])
 			i += n
 			v2, n := binary.Uvarint(data[i:])
 			i += n
-			fmt.Printf("[%v,%v]\n", v1, v2)
 			s.nakRegions.Nak(int64(v1), int64(v2))
 		}
 		// Start sending back at last ACK:
